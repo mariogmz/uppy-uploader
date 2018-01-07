9a7b7c6dc0478e25a879c6d6d6cfd209dd477003
Mario Gómez <mariogomezmtz@gmail.com>
2018-01-07 05:24:20 -0600

    Add some info

--- app/javascript/components/UppyApp.js
+++ app/javascript/components/UppyApp.js
@@ -48,7 +48,7 @@ class UppyApp extends React.Component {
 
   showMessage() {
     if(this.state.message !== undefined && this.state.message.length > 0) {
-      return <p>
+      return <p className="messages">
         {this.state.message}
       </p>
     }
@@ -67,6 +67,28 @@ class UppyApp extends React.Component {
             getUppyInstance={this.getUppyInstance}
             isActive={this.state.activeExample ===  1} />
           {this.showMessage()}
+          <p>
+            A more customizable and complete example can be found at <a href="https://uppy.io/examples/dashboard/">www.uppy.io/examples/dashboard</a>
+          </p>
+          <p>
+            There is a lot of options to fully customize uppy, also includes integrations:
+            <ul>
+              <li> Upload from local disk </li>
+              <li> Take photo from webcam </li>
+              <li> Google Drive </li>
+              <li> Dropbox </li>
+              <li> Instagram </li>
+            </ul>
+          </p>
+          <p> Also we have the possibility to upload the files directly to the Rails backend or
+              either use an open protocol like <a href="https://tus.io"> Tus </a> if we want to
+              support resumable file uploads
+          </p>
+          <p> Integrations with AwsS3 Buckets are possible </p>
+          <p> We can customize which files are allowed on each separated uploader, restrict file sizes,
+              use localization, custom messages for some case, and also there are React-Ready components
+              to integrate from scratch or as an addition to an already existent project.
+          </p>
       </section>
     );
   }
